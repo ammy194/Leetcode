7546f99ed5c7de6ab11167b126ac3a9532b1b058
Amartya_19 <65552026+ammy194@users.noreply.github.com>
2026-08-22 23:28:06 +0530

    Easy

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,15 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        int sum = 0;
         int n = nums.size();
-
-        for(int i = 0; i < n; i++){
-            sum += nums[i];
+        unordered_map<int,int>mp;
+        for(int i =0;i<n;i++){
+            mp[nums[i]]++;
         }
+        for(int i =0;i<=n;i++){
+            if(mp[i]==0){
+                return i;
+            }
+        }
+        return -1;
 
-        int final = n * (n + 1) / 2;
-
-        return final - sum;
+        
     }
 };

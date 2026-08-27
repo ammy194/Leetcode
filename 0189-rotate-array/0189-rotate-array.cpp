a47757f58fa9f05fea862ce0cd8575edf6200859
Amartya_19 <65552026+ammy194@users.noreply.github.com>
2026-08-28 00:32:30 +0530

    Medium

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,18 +1,11 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-
         int n = nums.size();
-
-        k = k % n;
-
-        // Reverse the whole array
-        reverse(nums.begin(), nums.end());
-
-        // Reverse first k elements
-        reverse(nums.begin(), nums.begin() + k);
-
-        // Reverse remaining elements
-        reverse(nums.begin() + k, nums.end());
+        k = k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };

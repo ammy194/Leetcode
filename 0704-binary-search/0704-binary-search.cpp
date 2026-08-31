74ef0e373aa2603bf8383a3fb563d44a5dc119af
Amartya_19 <65552026+ammy194@users.noreply.github.com>
2026-08-31 18:05:37 +0530

    Easy

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -2,27 +2,21 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int n = nums.size();
-
         int low = 0;
-        int high = n - 1;
-
-        while(low <= high) {
-
-            int mid = (low + high) / 2;
-
-            if(nums[mid] == target) {
+        int high = n-1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
                 return mid;
             }
-            else {
-                if(target > nums[mid]) {
-                    low = mid + 1;
-                }
-                else {
-                    high = mid - 1;
-                }
+            else if(nums[mid]<target){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
             }
         }
-
         return -1;
+        
     }
 };

fc5c346609355f03fcc0556065f8a00f876fc4f6
Amartya_19 <65552026+ammy194@users.noreply.github.com>
2026-09-15 19:02:02 +0530

    Medium

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -2,29 +2,22 @@ class Solution {
 public:
     vector<int> rearrangeArray(vector<int>& nums) {
         int n = nums.size();
+        vector<int> ans(n);
 
-        int pos[n/2];
-        int neg[n/2];
-
-        int p = 0;
-        int ne = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] > 0) {
-                pos[p] = nums[i];
-                p++;
+        int pos = 0;
+        int neg = 1;
+        for(int i = 0;i<n; i++){
+            if(nums[i]<0){
+                ans[neg] = nums[i];
+                neg+=2;
             }
-            else {
-                neg[ne] = nums[i];
-                ne++;
+            else{
+                ans[pos] = nums[i];
+                pos+=2;
             }
-        }
 
-        for(int i = 0; i < n/2; i++) {
-            nums[2*i] = pos[i];
-            nums[2*i+1] = neg[i];
         }
-
-        return nums;
+        return ans;
+        
     }
 };
